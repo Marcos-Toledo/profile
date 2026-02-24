65390793c45b51e0abdaae8ecf3c221bbfb885cf
Marcos-Toledo <xquinhox@gmail.com>
2026-02-24 15:41:45 -0300

    Remove static output configuration from Astro settings and update site URL for deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,6 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   // Altere para a URL real do seu site em produção
-  output: "static",
   site: "https://marcostoledo.vercel.app/",
 
   vite: {
